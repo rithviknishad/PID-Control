83b305a79ac48d2a05d5b3f3643b265276a0c050
Rithvik Nishad <rithviknishad@outlook.com>
2019-09-22 22:59:36 +0530

    Update PID.h

--- PID.h
+++ PID.h
@@ -4,31 +4,33 @@
 //  VERSION:    0.1.0
 //  DESC:       PID Control Loop Mechanism
 //  TAGS:       PID, Closed Control Loop, Open Source, Standard Library
-//  URL:        
+//  URL:        https://github.com/rithviknishad/PID-Control.git
 //
 
 class PID
 {
     protected:
     
-    float* control_variable;
-    float* process_variable;
-    
-    float error, last_error;
-
+    float* control_variable, process_variable, output_variable;
+    float error[];
 
     public:
 
-    float Kp = 0.1, Ki = 0.1, Kd = 0.1;
-
+    // PID tuning variables.
+    float Kp = 0.1, Ki = 0.1, Kd = 0.1; // set value to 0, to disable appropriate control mechanism
 
 
     protected:
 
+    static const float compute_p(float &error, float &kp) 
+    { return error * kp; }
 
+    static const float compute_i(float &accumulation_of_error, float &delta_time)
+    { return accumulation_of_error * delta_time; }
+
+    static const float compute_d(float &error, float &last_error, float &delta_time) 
+    { return (error - last_error) / delta_time; }
 
     public:
-
-
-
+    
 };
